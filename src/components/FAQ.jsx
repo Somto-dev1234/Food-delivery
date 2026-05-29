ace01a765042c3632fbdc01276cb7729dda8377d
somto-dev1234 <egbujorifeanyisomto@gmail.com>
2026-05-29 14:11:18 +0100

    I added faq to it

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import faqImg from "../assets/faq.png";
 import "./FAQ.css";
 
 export default function FAQ() {
+  const [showFAQ, setShowFAQ] = useState(false);
+
   return (
-    
     <section id="faq" className="faqSection">
       <div className="faqContainer">
         <div className="faqLeft">
@@ -11,16 +13,57 @@ export default function FAQ() {
         </div>
 
         <div className="faqRight">
-          <h2 className="faqHeadline">Order online with our simple checkout.</h2>
+          <h2 className="faqHeadline">
+            Order online with our simple checkout.
+          </h2>
 
           <p className="faqText">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500.
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500.
           </p>
 
-          <button className="faqButton" type="button">
-            See our FAQ
+          <button
+            className="faqButton"
+            type="button"
+            onClick={() => setShowFAQ(!showFAQ)}
+          >
+            {showFAQ ? "Hide FAQ" : "See our FAQ"}
           </button>
+
+          {showFAQ && (
+            <div className="faqBox">
+              <div className="faqItem">
+                <h4>How do I place an order?</h4>
+                <p>
+                  Simply browse our menu, add items to your cart, and proceed
+                  to checkout.
+                </p>
+              </div>
+
+              <div className="faqItem">
+                <h4>How long does delivery take?</h4>
+                <p>
+                  Delivery usually takes between 20–45 minutes depending on
+                  your location.
+                </p>
+              </div>
+
+              <div className="faqItem">
+                <h4>Can I track my order?</h4>
+                <p>
+                  Yes, you can track your order in real time after checkout.
+                </p>
+              </div>
+
+              <div className="faqItem">
+                <h4>What payment methods do you accept?</h4>
+                <p>
+                  We accept cards, bank transfer, and mobile wallet payments.
+                </p>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </section>
